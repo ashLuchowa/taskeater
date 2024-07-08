f87e913685fddebb1d91862cc68d72bba587d802
ashLuchowa <ashwinluchowa@gmail.com>
2024-07-08 23:20:03 +0800

    Add task and project classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import './style.scss';
 import _ from 'lodash';
 
+// Tasks
+class Tasks {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+let task001 = new Tasks('Implement skeleton structure', 'Create github repo, clone to desktop, create basic HTML/CSS/JS files.', 'task001');
+let task002 = new Tasks('Finish up visual css style', 'Use UI/UX practices to make the website appealing.', 'task002');
+let task003 = new Tasks('Link JavaScript', 'Create fade-in effect when scrolling down.', 'task003');
+
+const taskList = [task001, task002, task003];
+
+// Projects
 class Projects {
     constructor(title, description, task) {
         this.title = title;
@@ -9,8 +26,11 @@ class Projects {
     }
 }
 
-let project001 = new Projects('Implement skeleton structure', 'Create github repo, clone to desktop, create basic HTML/CSS/JS files.', 'task001');
+let project001 = new Projects('Portfolio 2024', 'I am working on this portfolio which will help me in finding a job.', taskList);
+let project002 = new Projects('Cleaning July', 'I have to clean the house before Christmas in July.', taskList);
+let project003 = new Projects('Link JavaScript', 'Create fade-in effect when scrolling down.', taskList);
 
-const projectList = [project001];
+// Project Lists Array
+const projectList = [project001, project002, project003];
 
-console.log(project001.title);
+console.log(project001.task);
