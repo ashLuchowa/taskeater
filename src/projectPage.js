6a9e686aac3d1c51609da859334e640aa9b144f6
ashLuchowa <ashwinluchowa@gmail.com>
2024-07-10 22:13:56 +0800

    Rename 'task' into 'options' + Delete unused HTML

--- src/projectPage.js
+++ src/projectPage.js
@@ -1,6 +1,6 @@
 const projectPage = (randomFunction) => {
-    // Task List
-    class Tasks {
+    // Option List
+    class Options {
         constructor(title, description, dueDate, priority, icon) {
             this.title = title;
             this.description = description;
@@ -9,16 +9,16 @@ const projectPage = (randomFunction) => {
             this.icon = icon;
         }
 
-        static task001 = new Tasks('Implement skeleton structure', 'Create github repo, clone to desktop, create basic HTML/CSS/JS files.', 'today', '1st', '!');
-        static task002 = new Tasks('Finish up visual css style', 'Use UI/UX practices to make the website appealing.', 'today', '2nd', '!');
-        static task003 = new Tasks('Link JavaScript', 'Create fade-in effect when scrolling down.', 'task003', 'today', '1st', '!');
+        static option001 = new Options('Implement skeleton structure', 'Create github repo, clone to desktop, create basic HTML/CSS/JS files.', 'today', '1st', '!');
+        static option002 = new Options('Finish up visual css style', 'Use UI/UX practices to make the website appealing.', 'today', '2nd', '!');
+        static option003 = new Options('Link JavaScript', 'Create fade-in effect when scrolling down.', 'option003', 'today', '1st', '!');
 
-        static taskList = [Tasks.task001, Tasks.task002, Tasks.task003];
+        static optionList = [Options.option001, Options.option002, Options.option003];
     }
 
-    Tasks.taskList.forEach((taskUnits) => {
-        randomFunction(taskUnits);
-        console.log(taskUnits.title);
+    Options.optionList.forEach((optionUnits) => {
+        randomFunction(optionUnits);
+        console.log(optionUnits.title);
     });
 
 
@@ -31,9 +31,9 @@ const projectPage = (randomFunction) => {
             this.icon = icon;
         }
 
-        static project001 = new Projects('Portfolio 2024', 'I am working on this portfolio which will help me in finding a job.', Tasks.taskList, '!');
-        static project002 = new Projects('Cleaning July', 'I have to clean the house before Christmas in July.', Tasks.taskList, '!');
-        static project003 = new Projects('Link JavaScript', 'Create fade-in effect when scrolling down.', Tasks.taskList, '!');
+        static project001 = new Projects('Portfolio 2024', 'I am working on this portfolio which will help me in finding a job.', Options.optionList, '!');
+        static project002 = new Projects('Cleaning July', 'I have to clean the house before Christmas in July.', Options.optionList, '!');
+        static project003 = new Projects('Link JavaScript', 'Create fade-in effect when scrolling down.', Options.optionList, '!');
 
         static projectList = [Projects.project001, Projects.project002, Projects.project003];
     }
--- src/sideBar.js
+++ src/sideBar.js
@@ -2,55 +2,54 @@ import projectPage from './projectPage.js';
 
 const sideBar = () => {
     const sideBar = document.querySelector('.sidebar');
+
     //-- Logo --//
-    const logoContainer = document.createElement('div');
-    logoContainer.classList.add('logo-container');
+    const logoContainer = document.querySelector('.logo-container');
     logoContainer.textContent = 'TaskEater';
     sideBar.appendChild(logoContainer);
 
-    //-- Task Container --//
-    const taskContainer = document.createElement('div');
-    taskContainer.classList.add('task-container');
-    // Add Task
-    const addTask = document.createElement('div');
-    addTask.classList.add('add-task');
-    // Add Task Icon
-    const addTaskIcon = document.createElement('div');
-    addTaskIcon.classList.add('add-task-icon');
-    addTaskIcon.textContent = '!';
-    // Add Task Title
-    const addTaskTitle = document.createElement('div');
-    addTaskTitle.textContent = 'Add Task';
-    addTaskTitle.classList.add('add-task-title');
-    addTask.appendChild(addTaskIcon);
-    addTask.appendChild(addTaskTitle);
-    taskContainer.appendChild(addTask);
-    sideBar.appendChild(taskContainer);
+    //-- Option Container --//
+    const optionContainer = document.querySelector('.option-container');
+    // Add Option
+    const addOption = document.createElement('div');
+    addOption.classList.add('add-option');
+    // Add Option Icon
+    const addOptionIcon = document.createElement('div');
+    addOptionIcon.classList.add('add-option-icon');
+    addOptionIcon.textContent = '!';
+    // Add Option Title
+    const addOptionTitle = document.createElement('div');
+    addOptionTitle.textContent = 'Add option';
+    addOptionTitle.classList.add('add-option-title');
+    addOption.appendChild(addOptionIcon);
+    addOption.appendChild(addOptionTitle);
+    optionContainer.appendChild(addOption);
+    sideBar.appendChild(optionContainer);
 
-    // Task List Container
-    const taskListContainer = document.createElement('div');
-    taskListContainer.classList.add('task-list-container');
-    taskContainer.appendChild(taskListContainer);
-    // Task Item
-    const randomFunction = (taskUnits) => {
-        const taskItem = document.createElement('div');
-        taskItem.classList.add('task-item');
-        taskListContainer.appendChild(taskItem);
-        // Task Item Icon
-        const taskItemIcon = document.createElement('div');
-        taskItemIcon.classList.add('task-item-icon');
-        taskItem.appendChild(taskItemIcon);
-        taskItemIcon.textContent = '!';
-        // Task Item Title
-        const taskItemTitle = document.createElement('div');
-        taskItemTitle.classList.add('task-item-title');
-        taskItem.appendChild(taskItemTitle);
-        taskItemTitle.textContent = taskUnits.title;
-        // Task Item Number
-        const taskItemNumber = document.createElement('div');
-        taskItemNumber.classList.add('task-item-number');
-        taskItem.appendChild(taskItemNumber);
-        taskItemNumber.textContent = 5;
+    // Option List Container
+    const optionListContainer = document.createElement('div');
+    optionListContainer.classList.add('option-list-container');
+    optionContainer.appendChild(optionListContainer);
+    // Option Item
+    const randomFunction = (optionUnits) => {
+        const optionItem = document.createElement('div');
+        optionItem.classList.add('option-item');
+        optionListContainer.appendChild(optionItem);
+        // Option Item Icon
+        const optionItemIcon = document.createElement('div');
+        optionItemIcon.classList.add('option-item-icon');
+        optionItem.appendChild(optionItemIcon);
+        optionItemIcon.textContent = '!';
+        // Option Item Title
+        const optionItemTitle = document.createElement('div');
+        optionItemTitle.classList.add('option-item-title');
+        optionItem.appendChild(optionItemTitle);
+        optionItemTitle.textContent = optionUnits.title;
+        // Option Item Number
+        const optionItemNumber = document.createElement('div');
+        optionItemNumber.classList.add('option-item-number');
+        optionItem.appendChild(optionItemNumber);
+        optionItemNumber.textContent = 5;
     }
 
 
